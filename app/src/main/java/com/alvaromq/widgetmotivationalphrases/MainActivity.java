90808d0a72ced72f4188c78bedf10dde2a4c46b1
alvaro <almamani@agetic.gob.bo>
2022-02-04 17:15:03 -0400

    perf: clean not sed files

--- app/src/main/java/com/alvaromq/widgetmotivationalphrases/MainActivity.java
+++ app/src/main/java/com/alvaromq/widgetmotivationalphrases/MainActivity.java
@@ -1,17 +1,9 @@
 package com.alvaromq.widgetmotivationalphrases;
-
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.alvaromq.widgetmotivationalphrases.databinding.ActivityMainBinding;
 
@@ -32,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,12 +54,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
