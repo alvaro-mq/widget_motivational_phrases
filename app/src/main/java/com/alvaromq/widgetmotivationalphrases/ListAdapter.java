be44ff2a5c1a4a35564429191e10c4950640f257
alvaro <almamani@agetic.gob.bo>
2022-03-07 22:43:33 -0400

    fix: fix import

--- app/src/main/java/com/alvaromq/widgetmotivationalphrases/ListAdapter.java
+++ app/src/main/java/com/alvaromq/widgetmotivationalphrases/ListAdapter.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.alvaromq.widgetmotivationalphrases.database.Phrase;
 import java.util.ArrayList;
-
-
-
 
 public class ListAdapter extends ArrayAdapter<Phrase> {
 
